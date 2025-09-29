42445e6a5cdb167715a996db12c511bfff227fee
BombardiloRuslanillo <ruslanjolmuradov@gmail.com>
2025-09-29 10:33:58 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const nbor = ['non','banan','mashina']
+let nbor1 = ('n harfi ishtirok etgan sozlar')
+console.log(nbor,nbor1)
+const nbor2 = ['asal','olma','aziza','gul']
+let nbor3 = ('n harfi ishtirok etmagan sozlar')
+console.log(nbor2,nbor3)
